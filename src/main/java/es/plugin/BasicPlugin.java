0b913d98831eeee6e096dddeb9b8ca013f4929dd
greek <chinagrowing@yeah.net>
2018-01-01 15:35:57 +0800

    add BasicPlugin

---
+++ src/main/java/es/plugin/BasicPlugin.java
@@ -0,0 +1,13 @@
+package es.plugin;
+
+import org.elasticsearch.plugins.Plugin;
+
+/**
+ * Copyright (C) 2015 - 2018 MICROSCENE Inc., All Rights Reserved.
+ */
+public class BasicPlugin extends Plugin {
+    public BasicPlugin() {
+        super();
+        // System.out.println("Create the Basic Plugin and installed it into elasticsearch");
+    }
+}
